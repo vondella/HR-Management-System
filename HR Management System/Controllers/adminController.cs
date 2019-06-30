8ebf55eaacadc827ba4e3f899e17454d7b39c11d
Al-Amin <30406265+al-amin1@users.noreply.github.com>
2019-06-30 14:24:03 +0600

    All Navigation Done
    Design required payment & attendence & profile and some of file.

--- HR Management System/Controllers/adminController.cs
+++ HR Management System/Controllers/adminController.cs
@@ -36,6 +36,10 @@ namespace HR_Management_System.Controllers
         {
             return RedirectToPage("/LeaveCategory");
         }
+        public IActionResult HolidayList()
+        {
+            return RedirectToPage("/HolidayList");
+        }
         public IActionResult AddDepartment()
         {
             return RedirectToPage("/AddDepartment");
@@ -47,6 +51,80 @@ namespace HR_Management_System.Controllers
         public IActionResult AddEmployee()
         {
             return RedirectToPage("/AddEmployee");
+        }
+
+        public IActionResult EmployeeList()
+        {
+            return RedirectToPage("/EmployeeList");
+        }
+
+        public IActionResult ManageAttendence()
+        {
+            return RedirectToPage("/ManageAttendence");
+        }
+
+        public IActionResult AttandenceReport()
+        {
+            return RedirectToPage("/AttandenceReport");
+        }
+
+        public IActionResult LeaveApplicationList()
+        {
+            return RedirectToPage("/LeaveApplicationList");
+        }
+
+        public IActionResult ManageSalaryDetails()
+        {
+            return RedirectToPage("/ManageSalaryDetails");
+        }
+        public IActionResult EmployeeSalaryList()
+        {
+            return RedirectToPage("/EmployeeSalaryList");
+        }
+        public IActionResult MakePayment()
+        {
+            return RedirectToPage("/MakePayment");
+        }
+
+        public IActionResult GeneratePayslip()
+        {
+            return RedirectToPage("/GeneratePayslip");
+        }
+
+        public IActionResult AddNotice()
+        {
+            return RedirectToPage("/AddNotice");
+        }
+        
+
+        public IActionResult ManageNotice()
+        {
+            return RedirectToPage("/ManageNotice");
+        }
+
+        public IActionResult ManageRecruitmentNotice()
+        {
+            return RedirectToPage("/ManageRecruitmentNotice");
+        }
+
+        public IActionResult AddRecruitmentNotice()
+        {
+            return RedirectToPage("/AddRecruitementNotice");
+        }
+
+        public IActionResult Recruitment_Applicant_List()
+        {
+            return RedirectToPage("/Recruitment_Applicant_List");
+        }
+
+        public IActionResult Selected_Applicant_List()
+        {
+            return RedirectToPage("/Selected_Applicant_List");
+        }
+
+        public IActionResult Profile()
+        {
+            return RedirectToPage("/Profile");
         }
     }
 }
