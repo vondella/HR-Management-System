d9cb1c4e6ee57c2813863e12538de7ac9e4b34f4
Al-Amin <30406265+al-amin1@users.noreply.github.com>
2019-08-17 12:37:05 +0600

    all design done

--- HR Management System/Pages/EmployeePages/Employee_Dashboard.cshtml.cs
+++ HR Management System/Pages/EmployeePages/Employee_Dashboard.cshtml.cs
@@ -4,13 +4,49 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using HR_Management_System.Data;
+using HR_Management_System.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace HR_Management_System.Pages
 {
     public class Employee_DashboardModel : PageModel
     {
-        public void OnGet()
+        public Employee_DashboardModel(HRMS_DB_Context db)
         {
+            _db = db;
+        }
+
+        readonly HRMS_DB_Context _db;
+
+
+        [BindProperty]
+        public List<Notice> Notices { get; set; }
+
+
+
+
+        public void OnGet(string src_string)
+        {
+            if (src_string != null && src_string != "")
+            {
+                Notices = _db.Notices.Where(a => a.Title.IndexOf(src_string, StringComparison.CurrentCultureIgnoreCase) > -1).AsNoTracking().ToList();
+            }
+            else
+            {
+                Notices = _db.Notices.AsNoTracking().ToList();
+            }
+        }
+
+
+
+
+        public async Task<IActionResult> OnGetViewNoticeAsync(long id)
+        {
+            var notice = await _db.Notices.FindAsync(id);
+            if (notice != null)
+                return RedirectToPage("/EmployeePages/emp_notice_details", notice);
+            return Page();
 
         }
     }
